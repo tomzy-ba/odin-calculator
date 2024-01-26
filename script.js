893ffbfdffff805cfc72afc30ae81db087ba33c5
tomzy-ba <trbee100@gmail.com>
2024-01-26 20:50:03 +0000

    FINISHED! YAY!

--- script.js
+++ script.js
@@ -45,16 +45,17 @@ divideButton.addEventListener("click", () => operatorButtons("divide"));
 
 function operatorButtons(operatorPressed) {
     if(currentOperator != null && currentValue != null) {
-        calcOperate(currentOperator, parseInt(calcInput.value), parseInt(currentValue))
+        calcOperate(currentOperator, parseInt(currentValue), parseInt(calcInput.value))
         currentOperator = "";
     }
     currentValue = calcInput.value;
     calcInput.value = null;
     currentOperator += operatorPressed;
+    currentValueDiv.innerHTML = currentValue;
 }
 
 equalsButton.addEventListener("click", () => {
-    calcOperate(currentOperator, parseInt(calcInput.value), parseInt(currentValue))
+    calcOperate(currentOperator, parseInt(currentValue), parseInt(calcInput.value))
 })
 
 function calcOperate(inputOperator, calcvalue1, calcvalue2) {
